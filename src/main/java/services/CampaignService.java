07a7fdcab1d50d5a3b9e272955fa107ca1116ddd
Antonio Arciniega <antarccub@gmail.com>
2016-11-14 21:34:44 +0100

    Added 'isActive()' and 'hasPassed()' methods to CampaignService

--- src/main/java/services/CampaignService.java
+++ src/main/java/services/CampaignService.java
@@ -1,9 +1,16 @@
 package services;
 
+import domain.Campaign;
+import domain.Sponsor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Assert;
 import repositories.CampaignRepository;
+
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
 
 @Service
 @Transactional
@@ -12,7 +19,79 @@ public class CampaignService {
     @Autowired
     private CampaignRepository campaignRepository;
 
+    @Autowired
+    private ActorService actorService;
+
     public CampaignService(){
         super();
+    }
+
+
+    public Campaign create(Campaign c){
+
+        return this.save(c);
+
+    }
+
+    private Campaign save(Campaign c){
+
+        Assert.notNull(c);
+
+
+        c.setSponsor(this.getActualSponsor());
+
+        return campaignRepository.save(c);
+
+    }
+
+    public Campaign modify(int ID, Campaign c){
+
+
+        Assert.isTrue(c.getSponsor().equals(this.getActualSponsor()));
+        Assert.isTrue(!isActive(c) && !hasPassed(c));
+
+        Campaign campaign = campaignRepository.findOne(ID);
+
+        campaign.setBanners(c.getBanners());
+        campaign.setEnd_at(c.getEnd_at());
+        campaign.setStart_at(c.getStart_at());
+        campaign.setStar_campaign(c.isStar_campaign());
+
+        return this.save(campaign);
+    }
+
+
+    public void delete(int ID){
+
+        Campaign campaign = campaignRepository.findOne(ID);
+
+        Assert.isTrue(campaign.getSponsor().equals(this.getActualSponsor()));
+        Assert.isTrue(!isActive(campaign) && !hasPassed(campaign));
+
+        campaignRepository.delete(ID);
+
+    }
+
+    public Collection<Campaign> list(){
+
+        return this.getActualSponsor().getCampaigns();
+
+    }
+
+    private Sponsor getActualSponsor(){
+
+        return (Sponsor) actorService.findByPrincipal();
+    }
+
+    private boolean isActive(Campaign c){
+
+        Date actualDate = new Date();
+        return c.getStart_at().before(actualDate) && c.getEnd_at().after(actualDate);
+    }
+
+    private boolean hasPassed(Campaign c){
+
+        Date actualDate = new Date();
+        return c.getStart_at().before(actualDate);
     }
 }
